9108277a734aa5c5509ea992ffb05e32a3548f27
rei-rala <ramonirala@outlook.com>
2021-09-13 22:29:43 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { Theme } from './contexts/Theme'
 import { UserSession } from './contexts/UserSession'
 
 import Header from './components/Header/Header'
-import Login from './components/Sections/Login/Login'
-import Logout from './components/Sections/Logout/Logout'
+//import Login from './components/Sections/Login/Login'
+//import Logout from './components/Sections/Logout/Logout'
 import Palabrerio from './components/Palabrerio/Palabrerio'
 
 const App = () => {
